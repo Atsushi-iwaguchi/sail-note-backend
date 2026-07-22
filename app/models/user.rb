ea93cdd71ac5c25df9ca561523315b86d619c3d4
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-07-22 13:55:53 +0900

    認証システムと新規登録の処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 
     enum :role, { member: 0, leader: 1 }
