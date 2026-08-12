8d2452fb8c71f62d039371d08637e8529d3c3f68
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-08-12 12:44:57 +0900

    Configure CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http://localhost:5173",
+           "https://sail-note-frontend.vercel.app"
 
-    resource "*",
+    resource "/api/*",
       headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
